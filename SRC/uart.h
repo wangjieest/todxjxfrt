c9ebd150e8a802e51dc9d417646dbcc47c6bd5ca
sev <wangjieest@163.com>
2013-12-14 12:18:15 +0800

    delete all

--- SRC/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-
-
-void UploadBinaries(unsigned char *buf, unsigned long len);
-void DownloadBinaries(unsigned char *buf, unsigned long len);
-
-#endif // __UART_H__
